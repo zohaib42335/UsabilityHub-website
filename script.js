0b4f256d325de82cfb837526de2274a37ab911ce
zohaib Hassan <zohaib423356@gmail.com>
2025-07-22 22:56:41 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//  const navToggle = document.querySelector(".nav-toggle");
+//   const navLinks = document.querySelector(".nav-links");
+
+//   navToggle.addEventListener("click", () => {
+//     navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+//   });
+

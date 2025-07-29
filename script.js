b7486f727f72ffa9c569b1fc519f89c56d9381be
Zohaib <107434445+zohaib42335@users.noreply.github.com>
2025-07-29 23:15:26 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-//  const navToggle = document.querySelector(".nav-toggle");
-//   const navLinks = document.querySelector(".nav-links");
-
-//   navToggle.addEventListener("click", () => {
-//     navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
-//   });
-
